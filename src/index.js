71fe7756f9694730c4e01bec27ba6308e8838c27
rylee816 <65700028+rylee816@users.noreply.github.com>
2020-09-24 14:26:32 -0400

    Add bacground image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "../public/styles.css";
 
 ReactDom.render(
   <div>
@@ -8,7 +9,7 @@ ReactDom.render(
       <li>He's the goodest boy.</li>
       <li>He has chubby paws.</li>
       <li>He yells at me a lot.</li>
-      <img style={{ width: 300 }} src="../images/Bizzle.jpg" alt="cat"></img>
+      <img style={{ width: 300 }} src="../images/Bizzle.jpg" alt=""></img>
     </ul>
   </div>,
   document.getElementById("root")
