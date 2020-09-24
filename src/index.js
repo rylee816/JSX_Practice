9d189dc965cc6b54e4195d118dbc314acf298fa0
rylee816 <65700028+rylee816@users.noreply.github.com>
2020-09-24 13:48:23 -0400

    Import React and add h1 and unordered list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Woozle kitten!</h1>
+    <ul>
+      <li>He's the goodest boy.</li>
+      <li>He has chubby paws.</li>
+      <li>He yells at me a lot.</li>
+      <img style={{ width: 300 }} src="../images/Bizzle.jpg" alt="cat"></img>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
